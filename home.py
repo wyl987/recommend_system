23ee377bde20d5c2e108130bb6b2e393dfe8d521
wyl987 <wyl987@uowmail.edu.au>
2025-02-14 17:27:44 +1100

    created mongodb methods and embeddings

--- home.py
+++ home.py
@@ -1,13 +1,15 @@
 import streamlit as st
 import pymongo
 import openai
+import pickle
+from bson.binary import Binary
 
 
 mongo_URI = st.secrets["mongo_URI"]
 
 # 1. Create mongodb connection
-client = pymongo.MongoClient(mongo_URI)
-db = client["recommend_system"]
+db_client = pymongo.MongoClient(mongo_URI)
+db = db_client["recommend_system"]
 # like a table in SQL
 collection = db["responses"]
 
@@ -22,12 +24,31 @@ st.image('https://img.freepik.com/free-vector/couple-romantic-penguins-celebrati
 
 st.subheader('Fill the questions so you can get a match!')
 
+name = st.text_input('What is your name?')
 hobbies = st.text_input('What are your top three interests or hobbies?')
 new_hobbies = st.text_input('What’s one hobby or activity you’ve always wanted to try but haven’t yet?')
 cuisine = st.text_input('What’s your favorite type of cuisine?')
 music = st.text_input('What type of music do you enjoy the most?')
-partner_traits = st.text_input('What are you looking for in a partner?')
-personalities = st.text_input('How would your friends describe your personality?')
+partner_traits = st.text_area('What are you looking for in a partner?')
+personalities = st.text_area('How would your friends describe your personality?')
+
+# 3. Generate embeddings from openai model
+def get_embeddings(text):
+  response = openai.embeddings.create(
+      input=text,
+      model="text-embedding-ada-002"
+  ) 
+  return response.data[0].embedding
+
+# 4. Save the responses to mongodb
+def save_response_to_db(response, embedding):
+  result = collection.insert_one({
+    "responses": response,
+    # Store embeddings as binary data
+    "embedding": Binary(pickle.dumps(embedding))
+  })
+  return result.inserted_id
+
 
 if st.button("Submit"):
   # recommendation function
